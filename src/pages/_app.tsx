3de887515bcfaea9d44a5fca7f1b750dc9f12902
gibify <gibifyofficial@gmail.com>
2021-11-21 00:55:43 -0300

    feat: add new buttons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Footer } from '../components/Footer';
+// import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 
 import '../styles/globals.css'
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
   <Header />
   <Component {...pageProps} />
 
-  <Footer />
+  {/* <Footer /> */}
   </>
   );
 }
--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -6,29 +6,33 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const data = [
     {
       id: uuidv4(),
-      title: 'Filé de Tilápia',
-      image: '/image/image.png',
-      origem: 'Santa Catarina',
+      title: '@pesquepaguegibicoski',
+      icon: '/icons/instagram-icon.svg',
+      url: 'https://www.instagram.com/pesquepaguegibicoski'
     },
     {
       id: uuidv4(),
-      title: 'Bebidas',
+      title: '@pesquepaguegibicoski',
+      icon: '/icons/facebook-icon.svg',
+      url: 'https://www.facebook.com/pesquepaguegibicoski',
     },
     {
       id: uuidv4(),
-      title: 'Porções',
+      title: '49 9 9180 9737',
+      icon: '/icons/whatsapp-icon.svg',
+      url: 'https://api.whatsapp.com/send?phone=+5549991809737'
     },
     {
       id: uuidv4(),
-      title: 'Vinhos',
+      title: '49 3433 6749',
+      icon: '/icons/phone-icon.svg',
+      url: 'tel:+55-49-3433-6749'
     },
     {
       id: uuidv4(),
-      title: 'Cachaça',
-    },
-    {
-      id: uuidv4(),
-      title: 'Salames',
+      title: 'Xanxerê - SC',
+      icon: '/icons/map-icon.svg',
+      url: 'https://www.google.com.br/maps/place/Pesque+Pague+Gibicoski/@-26.8615709,-52.4272246,18z/data=!4m5!3m4!1s0x94e4c3571804c567:0x2fd1422294e3c22c!8m2!3d-26.861669!4d-52.4261651?hl=en&authuser=0'
     },
   ];
   
